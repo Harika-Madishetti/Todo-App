9236ec04332afb27d19c9bcb6c47f26b8809fe8b
Harika-Raj <r121662@rguktrkv.ac.in>
2018-11-16 00:10:29 +0530

    Add the Tasks into the todo list

---
+++ src/main/resources/js/TodoItems.js
@@ -0,0 +1,18 @@
+import React,{ Component } from "react";
+
+class TodoItems extends Component {
+    createTasks(items)  {
+        return <li key={items.key}>{items.text}</li>
+    }
+    render() {
+        var todoEntries = this.props.entries;
+        var listItems = todoEntries.map(this.createTasks);
+
+        return(
+            <ul className="theList">
+                {listItems}
+            </ul>
+        );
+    }
+}
+export default TodoItems;
--- src/main/resources/js/TodoList.js
+++ src/main/resources/js/TodoList.js
@@ -1,15 +1,42 @@
 import React from "react";
+import TodoItems from "./TodoItems";
 
 class TodoList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items:[]
+        };
+        this.addItem = this.addItem.bind(this);
+    }
+
+    addItem(e) {
+        if(this._inputElement.value != "") {
+            var newItem = {
+                text : this._inputElement.value,
+                key : Date.now()
+            };
+            this.setState((prevState) => {
+                return {
+                    items:prevState.items.concat(newItem)
+                };
+            });
+        }
+        this._inputElement.value = "";
+        console.log(this.state.items);
+        e.preventDefault();
+    }
     render() {
         return (
             <div className="todolist">
                 <div className="header">
-                    <form>
-                        <input placeholder="What needs to be done?">
+                    <form  onSubmit={this.addItem}>
+                        <input ref={(a) => this._inputElement = a}
+                               placeholder="What needs to be done?">
                         </input>
                         <input type="hidden"></input>
                     </form>
+                    <TodoItems entries ={this.state.items}/>
                 </div>
             </div>
         );
